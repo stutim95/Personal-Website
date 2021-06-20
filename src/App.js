cc9a995bf3c5ba47f5bcc3b1da3c57c6c6c9195e
Stuti Misra <stutim95@gmail.com>
2021-06-20 00:16:06 -0400

    Remove unused vars

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import './App.css';
 import {Route, Switch, Link} from "react-router-dom";
 import About from "./components/About";
 import Home from "./components/Home";
-import {AppBar, Button, IconButton, Toolbar, Typography} from "@material-ui/core";
-import {classes} from "istanbul-lib-coverage";
+import {AppBar, Toolbar, Typography} from "@material-ui/core";
 
 function App() {
   return (
       <div>
           <AppBar position="static">
-              <Toolbar>
-                  <Typography variant="h6" className={classes.title}>
+              Stuti Misra 
+              <Toolbar variant="dense">
+                  <Typography variant="h6" color="inherit">
                       <Link to="/about">About</Link>
                   </Typography>
               </Toolbar>
